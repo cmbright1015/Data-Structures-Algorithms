305ce083b0ffa192be899a83f6dbb42a40f57aa9
Caleb Bright <143642889+cmbright1015@users.noreply.github.com>
2025-12-02 22:33:05 -0500

    QuickSort project

--- Bright_Caleb_QuickSort.cpp
+++ Bright_Caleb_QuickSort.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <chrono>
 #include <iomanip>
-#include <stdexcept>
+
 
 int medianThree(std::vector<double> &a, int left, int right) {
+    // middle index
     int middle = left + (right - left) / 2;
 
     // left <= middle <= right
@@ -20,12 +21,13 @@ int medianThree(std::vector<double> &a, int left, int right) {
         std::swap(a[middle], a[right]);
     }
 
-    // middle is moved to the left
+    // middle is moved to the left so we can use it as the pivot
     std::swap(a[left], a[middle]);
     return left;
 }
 
 int part(std::vector<double> &a, int left, int right) {
+    // if <2 just sort them
     if (right - left + 1 <= 2) {
         if (a[left] > a[right]) {
             std::swap(a[left], a[right]);
@@ -36,9 +38,12 @@ int part(std::vector<double> &a, int left, int right) {
     int pivotI = medianThree(a, left, right);
     double pivot = a[pivotI];
 
+    // move i to the right and move j to the left
     int i = left + 1;
     int j = right;
 
+    // move i forward if a[i] <= pivot and j has not been crossed
+    // then move j backwards if a[j] >= pivot and i has not been crossed
     while(true) {
         while (i <= j && a[i] <= pivot)
             ++i;
@@ -57,17 +62,21 @@ void quickSort(std::vector<double> &a, int left, int right) {
     if (left >= right) {
         return;
     }
+    // partition the array and find the pivot's final position
+    // sort the left and right parts using recursion
     int pivot_position = part(a, left, right);
     quickSort(a, left, pivot_position - 1);
     quickSort(a, pivot_position + 1, right);
 }
 
 int main(int argc, char *argv[]) {
+    // program, input file, output file
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " input.txt output.txt\n";
         return 1;
     }
 
+    // get input and output from the arugments
     const char *inputFileName = argv[1];
     const char *outputFileName = argv[2];
 
@@ -80,16 +89,20 @@ int main(int argc, char *argv[]) {
     std::vector<double> data;
     double value;
 
+    // reading the file until it fails
     while (inFile >> value) {
         data.push_back(value);
     }
     inFile.close();
 
+    // if the reading didn't end because of EOF there was a different error
     if (!inFile.eof()) {
         std::cerr << "Failed reading input file\n";
         return 1;
     }
 
+    // record time before and after
+    // sort if there is at least one item
     auto start = std::chrono::high_resolution_clock::now();
     if (!data.empty()) {
         quickSort(data, 0, static_cast<int>(data.size()) - 1);
@@ -98,12 +111,15 @@ int main(int argc, char *argv[]) {
 
     auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
+    // output file open fail
     std::ofstream outFile(outputFileName);
     if (!outFile) {
         std::cerr << "Error opening output file '" << outputFileName << "'.\n";
         return 1;
     }
 
+    // 6 digits after the decimal point
+    // the sorted values are put into the output file
     outFile << std::fixed << std::setprecision(6);
     for (std::size_t i = 0; i < data.size(); ++i) {
         outFile << data[i];
